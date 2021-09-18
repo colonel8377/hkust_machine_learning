b2fc9d62871def9f2dfe5effaf5e9350a318cedf
colonel8377 <80370342+colonel8377@users.noreply.github.com>
2021-09-18 14:32:00 +0800

    1.feat(fp_growth_py3.py): add new file based on fp-py 2.feat(fp_growth_py3.py): add testing file

---
+++ hw/fp_tree/test.py
@@ -0,0 +1,28 @@
+import fp_growth_py3 as fpg
+
+# 数据集
+dataset = [
+    ['C', 'D'],
+    ['B', 'E', 'F'],
+    ['A', 'C', 'D'],
+    ['A', 'C', 'D'],
+    ['C', 'D'],
+    ['B', 'E', 'F'],
+    ['A', 'C', 'D'],
+    ['A', 'C', 'D'],
+]
+
+if __name__ == '__main__':
+    frequent_itemsets = fpg.find_frequent_itemsets(dataset, minimum_support=3, include_support=True)
+    print(type(frequent_itemsets))   # print type
+
+    result = []
+    for itemset, support in frequent_itemsets:    # 将generator结果存入list
+        result.append((itemset, support))
+
+    result = sorted(result, key=lambda i: i[0])   # 排序后输出
+    for itemset, support in result:
+        print(str(itemset) + ' ' + str(support))
+
+
+
